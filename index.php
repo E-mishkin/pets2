39e8630a0908b0943e1a4c0b6b631eb48fa7f400
Alex Grigorenko <agrigorenko@mail.greenriver.edu>
2020-01-24 13:26:31 -0500

    Added the animals

--- index.php
+++ index.php
@@ -18,5 +18,38 @@ $f3->route('GET /', function (){
     echo "<a href='order'>Order a Pet</a>";
 });
 
+//Define a animal type route
+$f3->route("GET /@item", function($f3, $params){
+    var_dump($params);
+    $item = $params["item"];
+    echo "<p>You Ordered $item</p>";
+
+    $foodsWeServe = array("chicken", "dog", "cat", "owl", "snake");
+    if(!in_array($item, $foodsWeServe)){
+        echo "<p>Sorry... we don't serve $item</p>";
+    }
+
+    switch ($item){
+        case 'chicken':
+            echo "<p>Cluck!</p>";
+            break;
+        case 'dog':
+            echo "<p>Woof!</p>";
+            break;
+        case 'cat':
+            echo "<p>meow!</p>";
+            break;
+        case 'owl':
+            echo "<p>ooo!</p>";
+            break;
+        case 'snake':
+            echo "<p>hiss!</p>";
+            break;
+        default:
+            $f3->error(404);
+    }
+
+});
+
 //Run fat free
 $f3->run();
